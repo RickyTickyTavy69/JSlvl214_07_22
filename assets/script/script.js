9f4e3d2f986a85186701ac2024ecd437dd93b521
artem <bakirovartem69@gmail.com>
2022-07-14 14:57:03 +0200

    first one

---
+++ assets/script/script.js
@@ -0,0 +1,22 @@
+a = 12;
+b = a * 3;
+console.log(b);
+
+document.write(b);
+console.log(document.URL);
+
+console.log("Hello World!");
+console.log("Anton");
+const firstName = "Anton";
+console.log(firstName);
+
+const age = 28;
+console.log(age);
+const job = "trainer";
+console.log(job);
+let married = true;
+console.log(married);
+
+console.log(
+  `${firstName} ist ${age} Jahre alt, von Beruf ${job} und ist verheiratet : ${married}`
+);
